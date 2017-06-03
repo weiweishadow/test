56051423b3fac2e415712280347303a8761614c1
weiweishadow <weiweishadow@163.com>
2017-06-03 16:07:32 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,19 @@
-moudle.exports={
+var fs = require('fs');
+var list = [];
 
+moudle.exports={
+  add:function(article){
+    list.push(article);
+  },
+  del:function(index){
+    list.splice(index,1);
+  },
+  update:function(index,newArticle){
+    list.splice(index,1,newArticle);
+  },
+  find:function(){
+    return list;
+  },
+  store:function(){
+  }
 }
